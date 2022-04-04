b059c0e04291388c01cbfc24c9c2c65355d8e3ef
qingyuan-wu <qyw.wu@mail.utoronto.ca>
2022-04-03 22:19:11 -0400

    gitignore

--- Plastics-Tracker/toy_requests.py
+++ Plastics-Tracker/toy_requests.py
@@ -4,12 +4,10 @@ from random import randint
 PORT = 4003
 URL = f"http://localhost:{PORT}"
 
-data_obj = {"temp": "200", "brightness": "4334"}
-
 i = 0
 while True:
-    req.post(URL, data={ "type": randint(1,2)})
+    print(i)
+    req.post(URL, data={ "type": '1' })
     i += 1
     print(i, req)
     time.sleep(5)
-
